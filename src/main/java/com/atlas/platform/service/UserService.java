0b4de9dd1fc4bf1872c4cd43df94641e31e726e5
Anatolii Gorbenko <anatoliydrake@gmail.com>
2025-10-07 22:32:45 +0700

    fix: add existsByTelegramId method to UserRepository and create UserService

---
+++ src/main/java/com/atlas/platform/service/UserService.java
@@ -0,0 +1,59 @@
+package com.atlas.platform.service;
+
+import com.atlas.platform.entity.User;
+import com.atlas.platform.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public Optional<User> getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public Optional<User> getUserByTelegramId(Long telegramId) {
+        return userRepository.findByTelegramId(telegramId);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public boolean existsByUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    public boolean existsByTelegramId(Long telegramId) {
+        return userRepository.existsByTelegramId(telegramId);
+    }
+}

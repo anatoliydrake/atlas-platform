29097f997b18f0182af5a1fd82bd7134c6b070ee
Anatolii Gorbenko <anatoliydrake@gmail.com>
2025-10-07 20:54:36 +0700

    chore: initial Spring Boot project setup

---
+++ src/test/java/com/atlas/platform/AtlasPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlas.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlasPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
